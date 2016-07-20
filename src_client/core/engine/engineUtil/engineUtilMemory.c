463f39d09b94c11fbe53d7179bf27dcaad004df1
totetero <totetero@gmail.com>
2016-07-20 10:22:07 +0900

    独自メモリシステムのリファクタ

--- src_client/core/engine/engineUtil/engineUtilMemory.c
+++ src_client/core/engine/engineUtil/engineUtilMemory.c
@@ -17,34 +17,32 @@ static struct{
 // ----------------------------------------------------------------
 // ----------------------------------------------------------------
 
-// 独自mallocの実装
-void* engineUtilMemoryMallocImplement(char *info, size_t size){
+static void* addDatList(char *info, void *ptr){
+	if(ptr == NULL){trace("mem alloc error\n"); return NULL;}
 	// データ作成
 	struct engineUtilMemoryUnit *this = (struct engineUtilMemoryUnit*)calloc(1, sizeof(struct engineUtilMemoryUnit));
-	this->ptr = malloc(size);
+	this->ptr = ptr;
 	strncpy(this->info, info, sizeof(this->info));
 	// リストにデータ追加
 	this->next = localGlobal.datList;
 	localGlobal.datList = this;
 	// データを返す
-	return this->ptr;
+	return ptr;
+}
+
+// 独自mallocの実装
+void* engineUtilMemoryMallocImplement(char *info, size_t size){
+	return addDatList(info, malloc(size));
 }
 
 // 独自callocの実装
 void* engineUtilMemoryCallocImplement(char *info, size_t n, size_t size){
-	// データ作成
-	struct engineUtilMemoryUnit *this = (struct engineUtilMemoryUnit*)calloc(1, sizeof(struct engineUtilMemoryUnit));
-	this->ptr = calloc(n, size);
-	strncpy(this->info, info, sizeof(this->info));
-	// リストにデータ追加
-	this->next = localGlobal.datList;
-	localGlobal.datList = this;
-	// データを返す
-	return this->ptr;
+	return addDatList(info, calloc(n, size));
 }
 
 // 独自freeの実装
 void engineUtilMemoryFreeImplement(void *ptr){
+	int freeCount = 0;
 	struct engineUtilMemoryUnit *prev = NULL;
 	struct engineUtilMemoryUnit *temp = localGlobal.datList;
 	while(temp != NULL){
@@ -55,12 +53,16 @@ void engineUtilMemoryFreeImplement(void *ptr){
 			else{prev->next = temp->next;}
 			temp = temp->next;
 			// 解放
+			free(dispose->ptr);
 			free(dispose);
+			freeCount++;
 		}else{
 			prev = temp;
 			temp = temp->next;
 		}
 	}
+	if(freeCount < 1){trace("mem free error -- no such pointer\n");}
+	if(freeCount > 1){trace("mem free error -- too meny pointer\n");}
 }
 
 // 独自に確保したメモリ領域の確認
