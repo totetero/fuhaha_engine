14372241001a388f9c08085857238983d4ca4ec5
totetero <totetero@gmail.com>
2016-07-25 09:33:29 +0900

    独自領域確保のデータリスト構造変更

--- src_client/core/engine/engineUtil/engineUtil.h
+++ src_client/core/engine/engineUtil/engineUtil.h
@@ -10,23 +10,25 @@
 #ifdef FUHAHA_MEMORY_DEBUG
 void* engineUtilMemoryMallocImplement(char *info, size_t size);
 void* engineUtilMemoryCallocImplement(char *info, size_t n, size_t size);
-void engineUtilMemoryFreeImplement(void *ptr);
+void engineUtilMemoryFreeImplement(char *info, void *ptr);
 void engineUtilMemoryTraceImplement();
 #define ENGINEUTILMEMORY_STRINGIFY(n) #n
 #define ENGINEUTILMEMORY_TOSTRING(n) ENGINEUTILMEMORY_STRINGIFY(n) 
 #define ENGINEUTILMEMORY_LINEINFO __FILE__ ":" ENGINEUTILMEMORY_TOSTRING(__LINE__)
 #define engineUtilMemoryMalloc(size) engineUtilMemoryMallocImplement(ENGINEUTILMEMORY_LINEINFO, size)
 #define engineUtilMemoryCalloc(n, size) engineUtilMemoryCallocImplement(ENGINEUTILMEMORY_LINEINFO, n, size)
+#define engineUtilMemoryFree(ptr) engineUtilMemoryFreeImplement(ENGINEUTILMEMORY_LINEINFO, ptr)
 #define engineUtilMemoryInfoMalloc(info, size) engineUtilMemoryMallocImplement(info, size)
 #define engineUtilMemoryInfoCalloc(info, n, size) engineUtilMemoryCallocImplement(info, n, size)
-#define engineUtilMemoryFree(ptr) engineUtilMemoryFreeImplement(ptr)
+#define engineUtilMemoryInfoFree(info, ptr) engineUtilMemoryFreeImplement(info, ptr)
 #define engineUtilMemoryTrace() engineUtilMemoryTraceImplement()
 #else
 #define engineUtilMemoryMalloc(size) malloc(size)
 #define engineUtilMemoryCalloc(n, size) calloc(n, size)
+#define engineUtilMemoryFree(ptr) free(ptr)
 #define engineUtilMemoryInfoMalloc(info, size) malloc(size)
 #define engineUtilMemoryInfoCalloc(info, n, size) calloc(n, size)
-#define engineUtilMemoryFree(ptr) free(ptr)
+#define engineUtilMemoryInfoFree(info, ptr) free(ptr)
 #define engineUtilMemoryTrace()
 #endif
 
--- src_client/core/engine/engineUtil/engineUtilMemory.c
+++ src_client/core/engine/engineUtil/engineUtilMemory.c
@@ -7,72 +7,118 @@
 
 static struct{
 	struct engineUtilMemoryUnit{
-		struct engineUtilMemoryUnit *next;
 		void *ptr;
-		char info[64];
-	} *datList;
+		size_t size;
+		char info[128];
+		int count;
+	} **datList;
+	int datLength;
+	size_t debugSize;
 } localGlobal = {0};
 
 // ----------------------------------------------------------------
 // ----------------------------------------------------------------
 // ----------------------------------------------------------------
 
-static void* addDatList(char *info, void *ptr){
-	if(ptr == NULL){trace("mem alloc error\n"); return NULL;}
-	// データ作成
-	struct engineUtilMemoryUnit *this = (struct engineUtilMemoryUnit*)calloc(1, sizeof(struct engineUtilMemoryUnit));
-	this->ptr = ptr;
-	strncpy(this->info, info, sizeof(this->info));
-	// リストにデータ追加
-	this->next = localGlobal.datList;
-	localGlobal.datList = this;
-	// データを返す
+static void* addDatList(char *info, void *ptr, size_t size){
+	if(ptr == NULL){trace("mem alloc error %s\n", info); return NULL;}
+
+	// データの挿入先を探す
+	int index = -1;
+	// リストの空きを探す
+	for(int i = 0; i < localGlobal.datLength; i++){
+		struct engineUtilMemoryUnit *temp = localGlobal.datList[i];
+		if(temp == NULL || temp->ptr == NULL){index = i; break;}
+	}
+	if(index < 0){
+		// リストに空きがなかったらリスト拡張
+		index = localGlobal.datLength;
+		int addNum = 100;
+		int length = index + addNum;
+		void *list = calloc(length, sizeof(*localGlobal.datList));
+		if(localGlobal.datLength > 0){
+			memcpy(list, localGlobal.datList, localGlobal.datLength * sizeof(*localGlobal.datList));
+			free(localGlobal.datList);
+		}
+		localGlobal.datList = list;
+		localGlobal.datLength = length;
+		localGlobal.debugSize += addNum * sizeof(*localGlobal.datList);
+	}
+
+	// データ挿入
+	struct engineUtilMemoryUnit *unit = localGlobal.datList[index];
+	if(unit == NULL){
+		localGlobal.datList[index] = (struct engineUtilMemoryUnit*)calloc(1, sizeof(struct engineUtilMemoryUnit));
+		localGlobal.debugSize += sizeof(struct engineUtilMemoryUnit);
+		unit = localGlobal.datList[index];
+	}
+	unit->ptr = ptr;
+	unit->size = size;
+	strncpy(unit->info, info, sizeof(unit->info) - 1);
+	unit->info[sizeof(unit->info) - 1] = '\0';
+	unit->count = 0;
 	return ptr;
 }
 
 // 独自mallocの実装
 void* engineUtilMemoryMallocImplement(char *info, size_t size){
-	return addDatList(info, malloc(size));
+	if(size <= 0){trace("warning alloc size zero"); size = 1;}
+	return addDatList(info, malloc(size), size);
 }
 
 // 独自callocの実装
 void* engineUtilMemoryCallocImplement(char *info, size_t n, size_t size){
-	return addDatList(info, calloc(n, size));
+	if(size <= 0){trace("warning alloc size zero"); size = 1;}
+	return addDatList(info, calloc(n, size), n * size);
 }
 
 // 独自freeの実装
-void engineUtilMemoryFreeImplement(void *ptr){
+void engineUtilMemoryFreeImplement(char *info, void *ptr){
 	int freeCount = 0;
-	struct engineUtilMemoryUnit *prev = NULL;
-	struct engineUtilMemoryUnit *temp = localGlobal.datList;
-	while(temp != NULL){
-		if((void*)temp->ptr == ptr){
-			struct engineUtilMemoryUnit *dispose = temp;
-			// 次のを装填
-			if(prev == NULL){localGlobal.datList = temp->next;}
-			else{prev->next = temp->next;}
-			temp = temp->next;
-			// 解放
-			free(dispose->ptr);
-			free(dispose);
-			freeCount++;
-		}else{
-			prev = temp;
-			temp = temp->next;
-		}
+	for(int i = 0; i < localGlobal.datLength; i++){
+		struct engineUtilMemoryUnit *temp = localGlobal.datList[i];
+		if(temp == NULL || temp->ptr != ptr){continue;}
+		free(temp->ptr);
+		temp->ptr = NULL;
+		temp->size = 0;
+		freeCount++;
 	}
-	if(freeCount < 1){trace("mem free error -- no such pointer\n");}
-	if(freeCount > 1){trace("mem free error -- too meny pointer\n");}
+	if(freeCount < 1){trace("mem free error -- no such pointer %s\n", info);}
+	if(freeCount > 1){trace("mem free error -- too meny pointer %s\n", info);}
+}
+
+// 並べ替え関数
+static int datList_sort(const void *a, const void *b){
+	struct engineUtilMemoryUnit *dat1 = *(struct engineUtilMemoryUnit**)a;
+	struct engineUtilMemoryUnit *dat2 = *(struct engineUtilMemoryUnit**)b;
+	double val1 = (dat1 != NULL) ? dat1->count : 0;
+	double val2 = (dat2 != NULL) ? dat2->count : 0;
+	return (val1 - val2);
 }
 
 // 独自に確保したメモリ領域の確認
 void engineUtilMemoryTraceImplement(){
-	trace("mem trace\n");
-	struct engineUtilMemoryUnit *temp = localGlobal.datList;
-	while(temp != NULL){
-		trace("mem %s\n", temp->info);
-		temp = temp->next;
+	// メモリサイズ計算
+	size_t releaseSize = 0;
+	for(int i = 0; i < localGlobal.datLength; i++){
+		struct engineUtilMemoryUnit *temp = localGlobal.datList[i];
+		if(temp == NULL || temp->ptr == NULL){continue;}
+		releaseSize += temp->size;
 	}
+	char dstr[32];
+	char rstr[32];
+	snprintf(dstr, sizeof(dstr), "%d.%dkB", localGlobal.debugSize / 1000, localGlobal.debugSize % 1000);
+	snprintf(rstr, sizeof(rstr), "%d.%dkB", releaseSize / 1000, releaseSize % 1000);
+	trace("mem trace %s (+%s) ----------------\n", rstr, dstr);
+
+	qsort((void*)localGlobal.datList, localGlobal.datLength, sizeof(struct engineUtilMemoryUnit**), datList_sort);
+	for(int i = 0; i < localGlobal.datLength; i++){
+		struct engineUtilMemoryUnit *temp = localGlobal.datList[i];
+		if(temp == NULL || temp->ptr == NULL){continue;}
+		trace("mem %d %d %s\n", temp->count++, temp->size, temp->info);
+	}
+
+	trace("---------------- ----------------\n");
 }
 
 // ----------------------------------------------------------------
