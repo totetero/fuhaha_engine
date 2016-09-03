0270070ea6fed579823a1dec9141b84b2fa31591
totetero <totetero@gmail.com>
2016-09-03 12:54:47 +0900

    バージョン設定シェルスクリプト

--- setVersion.sh
+++ setVersion.sh
@@ -7,14 +7,14 @@ VERSIONNAME=0.0.1
 # --------------------------------
 
 # android id設定
-sed -i '' -e 's/^fuhahaApplicationId=.*/fuhahaApplicationId='${IDENTIFIER}'/g' src_platform/android/gradle.properties
+sed -i '' -e 's/project.ext.fuhahaApplicationId = ".*"/project.ext.fuhahaApplicationId = "'${IDENTIFIER}'"/g' src_platform/android/build.gradle
 
 # ios id設定
 sed -i '' -e 's/PRODUCT_BUNDLE_IDENTIFIER = .*;/PRODUCT_BUNDLE_IDENTIFIER = '${IDENTIFIER}';/g' src_platform/ios/fuhaha.xcodeproj/project.pbxproj
 
 # android version設定
-sed -i '' -e 's/^fuhahaVersionCode=.*/fuhahaVersionCode='${VERSIONCODE}'/g' src_platform/android/gradle.properties
-sed -i '' -e 's/^fuhahaVersionName=.*/fuhahaVersionName='${VERSIONNAME}'/g' src_platform/android/gradle.properties
+sed -i '' -e 's/project.ext.fuhahaVersionCode = .*/project.ext.fuhahaVersionCode = '${VERSIONCODE}'/g' src_platform/android/build.gradle
+sed -i '' -e 's/project.ext.fuhahaVersionName = ".*"/project.ext.fuhahaVersionName = "'${VERSIONNAME}'"/g' src_platform/android/build.gradle
 
 # ios version設定
 cat src_platform/ios/fuhaha/Info.plist | perl -e '
