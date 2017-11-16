13e6903b9d4c9d34e2ef8a10e88b60d7a3192fa5
totetero <totetero@gmail.com>
2017-11-17 02:08:57 +0900

    要素破棄時に親から切断

--- src_client/core/engine/engineLayout02/engineLayout02ViewPartsText.c
+++ src_client/core/engine/engineLayout02/engineLayout02ViewPartsText.c
@@ -374,8 +374,8 @@ static void dispose(struct engineLayout02ViewPartsTextImplement *this){
 	engineGraphicObjectVBODispose(this->egoIdTexc);
 	engineGraphicObjectIBODispose(this->egoIdFace);
 	engineGraphicTextureDispose(this->egoIdTexTest);
-	engineLayout02ViewUtilChildrenRemove(((struct engineLayout02View*)this)->children.parent, (struct engineLayout02View*)this);
 	engineLayout02ViewUtilPositionDispose((struct engineLayout02View*)this);
+	engineLayout02ViewDetouch((struct engineLayout02View*)this);
 	engineUtilMemoryInfoFree("engineLayout02ViewPartsText", this);
 }
 
