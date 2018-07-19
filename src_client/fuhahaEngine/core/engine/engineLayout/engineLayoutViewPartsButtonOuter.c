ffd833939464521fe2792eee7859d548d6178824
totetero <totetero@gmail.com>
2018-07-20 00:36:52 +0900

    外枠ボタンで孫要素は確認対象外にする

--- src_client/fuhahaEngine/core/engine/engineLayout/engineLayoutViewPartsButtonOuter.c
+++ src_client/fuhahaEngine/core/engine/engineLayout/engineLayoutViewPartsButtonOuter.c
@@ -13,22 +13,17 @@ struct engineLayoutViewPartsButtonOuterImplement{
 
 // ----------------------------------------------------------------
 
-// タッチ可能な子要素の外側にあることを再帰で確認する
-static bool isChildrenOuter(struct engineLayoutView *this, double x, double y){
-	// 子要素に対して処理を適用する
-	struct engineLayoutView *temp = this->family.childrenHead;
-	while(temp != NULL){
-		if(temp->interact.setting.isTouchable && temp->position.isInner(temp, x, y)){return false;}
-		if(!isChildrenOuter(temp, x, y)){return false;}
-		temp = temp->family.next;
-	}
-	// 子要素の内側ではなかった
-	return true;
-}
-
 // タッチ判定
 static bool isInner(struct engineLayoutViewPartsButtonOuterImplement *this, double x, double y){
-	return engineLayoutViewUtilPositionTransformIsInner((struct engineLayoutView*)this, x, y) && isChildrenOuter((struct engineLayoutView*)this, x, y);
+	// 自要素の内側にあることを確認する
+	if(!engineLayoutViewUtilPositionTransformIsInner((struct engineLayoutView*)this, x, y)){return false;}
+	// 子要素の外側にあることを確認する 孫要素は確認しない
+	struct engineLayoutView *temp = this->super.super.family.childrenHead;
+	while(temp != NULL){
+		if(temp->position.isInner(temp, x, y)){return false;}
+		temp = temp->family.next;
+	}
+	return true;
 }
 
 // 初期化
