f5f82413708be038a9baed5fb5ab549d7a43f0b0
totetero <totetero@gmail.com>
2018-09-11 09:20:34 +0900

    ぉぅぃぇ

--- src_client/main/core/page00test/pageTest2.c
+++ src_client/main/core/page00test/pageTest2.c
@@ -8,10 +8,10 @@
 // ページ状態初期化
 void pageTest2PushPage(){
 	// ページカートリッジ作成
-	struct engineCartridgeLayoutPage *cartridge = engineCartridgeLayoutPageCreate();
-	engineCartridgeLayoutPageSetView(cartridge, (struct engineLayoutView*)pageTest2PartsRootCreate(cartridge));
+	struct engineCartridgeLayoutPage *cartridgePage = engineCartridgeLayoutPageCreate();
+	engineCartridgeLayoutPageSetView(cartridgePage, (struct engineLayoutView*)pageTest2PartsRootCreate(cartridgePage));
 	// ページカートリッジ装填
-	engineCartridgePageManagerPush((struct engineCartridgePage*)cartridge);
+	engineCartridgePageManagerPush((struct engineCartridgePage*)cartridgePage);
 }
 
 // ----------------------------------------------------------------
